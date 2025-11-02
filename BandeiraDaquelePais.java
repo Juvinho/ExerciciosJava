d7cfedacc8d96d6aa1df5573fd6c2d7fad03d173
Juvinho <umjuan123@gmail.com>
2025-11-02 16:20:28 -0300

    Add files via upload

---
+++ BandeiraDaquelePais.java
@@ -0,0 +1,26 @@
+public class BandeiraDaquelePais {
+    public static void main(String[] args) {
+        System.out.println("*  *  *  *  *  *  " + "==".repeat(17));
+        System.out.println("  *  *  *  *  *   " + "==".repeat(17));
+
+        System.out.println("*  *  *  *  *  *  " + "==".repeat(17));
+        System.out.println("  *  *  *  *  *   " + "==".repeat(17));
+
+        System.out.println("*  *  *  *  *  *  " + "==".repeat(17));
+        System.out.println("  *  *  *  *  *   " + "==".repeat(17));
+
+        System.out.println("*  *  *  *  *  *  " + "==".repeat(17));
+        System.out.println("  *  *  *  *  *   " + "==".repeat(17));
+
+        System.out.println("*  *  *  *  *  *  " + "==".repeat(17));
+
+        System.out.println("==".repeat(26));
+        System.out.println("==".repeat(26));
+        System.out.println("==".repeat(26));
+        System.out.println("==".repeat(26));
+        System.out.println("==".repeat(26));
+        System.out.println("==".repeat(26));
+
+    }
+
+}
